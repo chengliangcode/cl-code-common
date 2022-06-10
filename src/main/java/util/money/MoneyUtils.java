92a52ab02ffcff3ce5040a3df4e711a0221c3fc6
chengl <714552682@qq.com>
2022-06-10 17:41:18 +0800

    commit

---
+++ src/main/java/util/money/MoneyUtils.java
@@ -0,0 +1,25 @@
+package util.money;
+
+/**
+ * @author chengliang
+ * @date 2022/6/9 10:28
+ */
+public class MoneyUtils {
+
+    public enum Unit {
+        // 分
+        FEN,
+        // 角
+        JIAO,
+        // 元
+        YUAN,
+        // 百
+        HUNDRED_YUAN,
+        // 千
+        THOUSAND_YUAN,
+        // 万
+
+    }
+
+
+}
--- src/main/java/util/time/TimestampUtils.java
+++ src/main/java/util/time/TimestampUtils.java
@@ -3,26 +3,40 @@ package util.time;
 import java.util.Calendar;
 
 /**
- * cl的时间戳工具
+ * chengliang的时间戳工具
  *
  * @author chengliang
  * @date 2022/6/8 16:18
  */
-public class TimeUtils {
+public class TimestampUtils {
 
+    /**
+     * 获取当前时间戳
+     *
+     * @return {@code Long}
+     */
     public static Long getCurrentTimestamp() {
         return System.currentTimeMillis();
     }
 
+    /**
+     * 获取今天起始时间戳
+     *
+     * @return {@code Long}
+     */
     public static Long getTodayTimestamp() {
-        return InterceptTimestamp(getCalendarInstance()).getTimeInMillis();
+        return interceptTimestamp(getCalendarInstance()).getTimeInMillis();
     }
 
+    /**
+     * 获取明天起始时间戳
+     *
+     * @return {@code Long}
+     */
     public static Long getTomorrowTimestamp() {
-
         Calendar calendar = getCalendarInstance();
         calendar.add(Calendar.DATE, 1);
-        return InterceptTimestamp(calendar).getTimeInMillis();
+        return interceptTimestamp(calendar).getTimeInMillis();
     }
 
     private static Calendar getCalendarInstance() {
@@ -31,7 +45,7 @@ public class TimeUtils {
         return calendar;
     }
 
-    private static Calendar InterceptTimestamp(Calendar calendar) {
+    private static Calendar interceptTimestamp(Calendar calendar) {
         calendar.set(Calendar.HOUR_OF_DAY, 0);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
